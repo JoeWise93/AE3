08ab8601bc912d0a25b8ab730cde4f5e9a9cb513
JoeWise93 <joseph.w.wiseman-1@ou.edu>
2025-03-16 16:32:05 -0500

    Update Task 1.R

--- Task 1.R
+++ Task 1.R
@@ -1,18 +1,18 @@
 install.packages("tidyverse")
 library(tidyverse)
-
+data <- read.csv("/cloud/project/nobel-prize-winners-1.csv", stringsAsFactors = FALSE,fileEncoding = "UTF-8")
 data <- read.csv("/cloud/project/nobel-prize-winners-1.csv", stringsAsFactors = FALSE, fileEncoding = "UTF-8")
 data <- data[!duplicated(data), ]
 colnames(data) <- tolower(gsub(" ", "_", colnames(data)))
 data$name <- gsub("[[:punct:]]", "", data$name)
-data <- data %>% select(-c(bornlocation, diedlocation, borncoordinates, diedcoordinates, 
-                           borncountry_now, borncity_now, diedcountry_now, diedcity_now))
-data <- data %>% rename(
-  borncountry = borncountry_original,
-  borncity = borncity_original,
+@@ -25,11 +26,9 @@ data <- data %>% rename(
   diedcity = diedcity_original,
   diedcountry = diedcountry_original
 )
+data[data == ""] <- NA
+data <- drop_na(data)
 data[is.na(data)] <- "Unknown"
 data[] <- lapply(data, function(x) if (is.character(x)) iconv(x, from = "UTF-8", to = "ASCII//TRANSLIT") else x)
+write.csv(data, "/cloud/project/nobel-prize-winners-1_cleaned.csv",
+          row.names = FALSE, fileEncoding = "UTF-8")
 write.csv(data, "/cloud/project/nobel-prize-winners-1_cleaned.csv", row.names = FALSE, fileEncoding = "UTF-8")
